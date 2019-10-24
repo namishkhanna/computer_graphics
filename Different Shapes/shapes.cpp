9da8e5ca1b034e7e91af827068dfb9b65a6f334f
Namish Khanna <38690317+namishkhanna@users.noreply.github.com>
2019-10-24 10:33:56 +0530

    Different Shapes

---
+++ Different Shapes/shapes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+#include<graphics.h>
+int main()
+{
+	int gd=DETECT,gm;
+	initgraph(&gd,&gm,"");
+
+	int l=100,t=100,r=200,b=200,x=300,y=150,rad=50;
+	rectangle(l,t,r,b);
+	circle(x,y,rad);
+	bar(l+300,t,r+300,b);
+	ellipse(x,y+200,-60,300,100,50);
+	line(l-10,t+150,l+140,b);
+	outtextxy(x-20,y,"hello");
+
+    getch();
+	closegraph();
+	return 0;
+}
